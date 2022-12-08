b3d80e3f71d43ad3bde0d163a617ef96446c3d98
JeanaeClark <jeclark@udel.edu>
2022-12-08 14:41:39 -0500

    Added instructions modal

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import ElementList from "./components/ElementList";
+import InstructionsModal from "./InstructionsModal";
 
 import "./App.css";
 //import Container from "./components/Container";
@@ -10,6 +11,7 @@ function App(): JSX.Element {
     return (
         <DndProvider backend={HTML5Backend}>
             <div className="App">
+                <InstructionsModal></InstructionsModal>
                 <ElementList></ElementList>
             </div>
         </DndProvider>
---
+++ src/InstructionsModal.tsx
@@ -0,0 +1,43 @@
+/* eslint-disable react/jsx-key */
+import React, { useState } from "react";
+import Modal from "react-bootstrap/Modal";
+
+function InstructionsModal() {
+    const [show, setShow] = useState(true);
+
+    const handleClose = () => setShow(false);
+
+    return (
+        <>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Welcome to ChemPAL</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <h3>What is ChemPAL?</h3>
+                    <p>
+                        ChemPAL is a tool designed for middle school/highschool
+                        Chemistry teachers to teach students about creating
+                        compounds
+                    </p>
+                    <h3> How do you play?</h3>
+                    <p>
+                        On the lefthand side of the screen, you will see a list
+                        of elements. Drag each element into the center that you
+                        would like to combine. To combine, place the first
+                        element on top of the second. To delete an element, drag
+                        it to the green trashcan.
+                    </p>
+                    <h5>
+                        Made by: Jeanae Clark, Nafisa Maryam, and Sara
+                        Heintzelman for CISC275: Introduction to Software
+                        Engineering
+                    </h5>
+                </Modal.Body>
+                <Modal.Footer></Modal.Footer>
+            </Modal>
+        </>
+    );
+}
+
+export default InstructionsModal;
