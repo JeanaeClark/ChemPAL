84e107652719cf9dfd21d7d3faeacc047dff4562
JeanaeClark <90004629+JeanaeClark@users.noreply.github.com>
2022-12-08 09:48:08 -0500

    Update elementList.ts

--- src/elementList.ts
+++ src/elementList.ts
@@ -1,5 +1,17 @@
 import { Element } from "./interfaces/Element";
 
+const Sodium: Element = {
+    name: "Sodium",
+    id: 16,
+    image: require("./images/11 sodium.png"),
+    chemAbr: "Na",
+    atomicNum: 11,
+    neededforCompound: ["Chlorine", "Sodium Chloride"],
+    shown: false,
+    left: 225,
+    top: 118
+};
+
 const Nitrogen: Element = {
     name: "Nitrogen",
     id: 1,
@@ -7,10 +19,8 @@ const Nitrogen: Element = {
     chemAbr: "N",
     atomicNum: 7,
     shown: false,
-    left: 50,
-    top: 50,
-    fact: "Nitrogen is odorless, tasteless, and colorless.",
-    compounds: ["Ammonia", "Nitrate", "Nitrous Oxide"]
+    left: 225,
+    top: 118
 };
 
 const Chlorine: Element = {
@@ -20,49 +30,44 @@ const Chlorine: Element = {
     chemAbr: "Cl",
     atomicNum: 17,
     shown: false,
-    left: 50,
-    top: 50,
-    fact: "Nitrogen is odorless, tasteless, and colorless.",
-    compounds: ["Ammonia", "Nitrate", "Nitrous Oxide"]
+    left: 225,
+    top: 118
 };
 
 const Potassium: Element = {
     name: "Potassium",
     id: 3,
     image: require("./images/19 potassium.png"),
+    neededforCompound: ["Chlorine", "Potassium Chloride"],
     chemAbr: "K",
     atomicNum: 19,
     shown: false,
-    left: 50,
-    top: 50,
-    fact: "Potassium is the second lightest (least dense) metal after lithium.",
-    compounds: ["Ammonia", "Nitrate", "Nitrous Oxide"]
+    left: 225,
+    top: 118
 };
 
 const Hydrogen: Element = {
     name: "Hydrogen",
     id: 4,
     image: require("./images/1 hydrogen.png"),
+    neededforCompound: ["Chlorine", "Hydrochloric Acid"],
     chemAbr: "H",
     atomicNum: 1,
     shown: false,
-    left: 50,
-    top: 50,
-    fact: "Hydrogen is the most abundant element.",
-    compounds: ["Ammonia", "Nitrate", "Nitrous Oxide"]
+    left: 225,
+    top: 118
 };
 
 const Silver: Element = {
     name: "Silver",
     id: 5,
     image: require("./images/47 silver.png"),
+    neededforCompound: ["Iodide", "Silver Iodide"],
     chemAbr: "Ag",
     atomicNum: 47,
     shown: false,
-    left: 50,
-    top: 50,
-    fact: "Silver can make it rain.",
-    compounds: ["Ammonia", "Nitrate", "Nitrous Oxide"]
+    left: 225,
+    top: 118
 };
 
 const Iodine: Element = {
@@ -72,36 +77,32 @@ const Iodine: Element = {
     chemAbr: "I",
     atomicNum: 53,
     shown: false,
-    left: 50,
-    top: 50,
-    fact: "Iodine was used to make the first photographs.",
-    compounds: ["Ammonia", "Nitrate", "Nitrous Oxide"]
+    left: 225,
+    top: 118
 };
 
 const Aluminum: Element = {
     name: "Aluminum",
     id: 7,
     image: require("./images/13 aluminum.png"),
+    neededforCompound: ["Nitrogen", "Aluminum Nitride"],
     chemAbr: "Al",
     atomicNum: 13,
     shown: false,
-    left: 50,
-    top: 50,
-    fact: "It doesn't rust.",
-    compounds: ["Ammonia", "Nitrate", "Nitrous Oxide"]
+    left: 225,
+    top: 118
 };
 
 const Cadmium: Element = {
     name: "Cadmium",
     id: 8,
     image: require("./images/48 cadmium.png"),
+    neededforCompound: ["Tellurium", "Cadmium Telluride"],
     chemAbr: "Cd",
     atomicNum: 48,
     shown: false,
-    left: 50,
-    top: 50,
-    fact: "Cadmium is used in many products, including batteries, plastics, and in cigarette smoke.",
-    compounds: ["Ammonia", "Nitrate", "Nitrous Oxide"]
+    left: 225,
+    top: 118
 };
 
 const Tellurium: Element = {
@@ -111,23 +112,20 @@ const Tellurium: Element = {
     chemAbr: "Te",
     atomicNum: 52,
     shown: false,
-    left: 50,
-    top: 50,
-    fact: "Tellurium is one of the rarest elements on Earth, but plentiful in space.",
-    compounds: ["Ammonia", "Nitrate", "Nitrous Oxide"]
+    left: 225,
+    top: 118
 };
 
 const Cesium: Element = {
     name: "Cesium",
     id: 10,
     image: require("./images/55 cesium.png"),
+    neededforCompound: ["Fluorine", "Cesium Fluoride"],
     chemAbr: "Cs",
     atomicNum: 55,
     shown: false,
-    left: 50,
-    top: 50,
-    fact: "Cesium is incredibly accurate at timekeeping and is used in atomic clocks.",
-    compounds: ["Ammonia", "Nitrate", "Nitrous Oxide"]
+    left: 225,
+    top: 118
 };
 
 const Fluorine: Element = {
@@ -137,23 +135,20 @@ const Fluorine: Element = {
     chemAbr: "Fl",
     atomicNum: 9,
     shown: false,
-    left: 50,
-    top: 50,
-    fact: " Fluorine is the only element which will shape compounds with noble gasses xenon, krypton, and radon.",
-    compounds: ["Ammonia", "Nitrate", "Nitrous Oxide"]
+    left: 225,
+    top: 118
 };
 
 const Carbon: Element = {
     name: "Carbon",
     id: 12,
     image: require("./images/6 carbon.png"),
+    neededforCompound: ["Oxygen", "Carbon Monoxide"],
     chemAbr: "C",
     atomicNum: 6,
     shown: false,
-    left: 50,
-    top: 50,
-    fact: "ALMOST A FIFTH OF YOUR BODY IS CARBON.",
-    compounds: ["Ammonia", "Nitrate", "Nitrous Oxide"]
+    left: 225,
+    top: 118
 };
 
 const Oxygen: Element = {
@@ -163,39 +158,36 @@ const Oxygen: Element = {
     chemAbr: "O",
     atomicNum: 8,
     shown: false,
-    left: 50,
-    top: 50,
-    fact: "Oxygen makes up around 21% of the Earth's atmosphere.",
-    compounds: ["Ammonia", "Nitrate", "Nitrous Oxide"]
+    left: 225,
+    top: 118
 };
 
 const Lithium: Element = {
     name: "Lithium",
     id: 14,
     image: require("./images/3 lithium.png"),
+    neededforCompound: ["Fluorine", "Lithium Fluoride"],
     chemAbr: "Li",
     atomicNum: 3,
     shown: false,
-    left: 50,
-    top: 50,
-    fact: "Although it is a metal, it is soft enough to cut with a knife.",
-    compounds: ["Ammonia", "Nitrate", "Nitrous Oxide"]
+    left: 225,
+    top: 118
 };
 
 const Silicon: Element = {
     name: "Silicon",
     id: 15,
     image: require("./images/14 silicon.png"),
+    neededforCompound: ["Carbon", "Silicon Carbide"],
     chemAbr: "Si",
     atomicNum: 14,
     shown: false,
-    left: 50,
-    top: 50,
-    fact: "Silicon is widely used in computer chips and solar cells.",
-    compounds: ["Ammonia", "Nitrate", "Nitrous Oxide"]
+    left: 225,
+    top: 118
 };
 
 export const elements = [
+    Sodium,
     Nitrogen,
     Chlorine,
     Potassium,
